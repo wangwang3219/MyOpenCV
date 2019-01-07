55e53b8e06eff40f39beeb604164dc8ebf9a304e
WangWang3219 <2459145973@qq.com>
2019-01-07 16:20:57 +0800

    1

---
+++ 1.py
@@ -0,0 +1,12 @@
+import cv2  
+import numpy as np  
+
+img = cv2.imread("E:/OpenCv exercise/a.jpg")  
+emptyImage = np.zeros(img.shape, np.uint8)  
+emptyImage2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)   
+cv2.imshow("EmptyImage", emptyImage)  
+cv2.imshow("Image", img)  
+cv2.imshow("EmptyImage2", emptyImage2)   
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
